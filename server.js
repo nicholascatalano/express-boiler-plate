ca12079180db17034730225ebe113d5606442d5e
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-20 15:02:13 -0500

    adds base express server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // DEPENDENCIES
+const express = require("express");
 
 // DATA
 
 // APP/PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // MIDDLEWARES
 
 // ROUTES
 
 // START APP
+app.listen(PORT, () => console.log(`Server live on port ${PORT}.`));
