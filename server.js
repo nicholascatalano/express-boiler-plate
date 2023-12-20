4a3aaa7f0cbbef027a5bad4e52795d779444eb75
nickcatalano5 <nickcatalano24@gmail.com>
2023-12-20 15:04:58 -0500

    adds middlewares for req.body and static assets

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // MIDDLEWARES
+// stuff to get the req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// static assets
+app.use(express.static("public"));
 
 // ROUTES
 
